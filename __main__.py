d3f1438e6489d9ab9a1d2b9a787f96a3621f009b
Asad Tariq <asadtariq96@gmail.com>
2021-09-16 17:43:25 +0300

    added index.html

--- __main__.py
+++ __main__.py
@@ -6,5 +6,11 @@ from pulumi_aws import s3
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('my-bucket-asad-pulumi')
 
+bucketObject = s3.BucketObject(
+    'index.html',
+    bucket=bucket,
+    source=pulumi.FileAsset('index.html')
+)
+
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
